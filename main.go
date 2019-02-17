5de1b0f164b483ce3af16ddd6d27d80d04d5dcc4
Andriy Orehov <andriyorehov@gmail.com>
2019-02-17 23:06:33 +0200

    add caesar cipher

--- main.go
+++ main.go
@@ -6,15 +6,71 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 type Atbash struct {
-	Name string `json:"name"`
+	Text string `json:"text"`
 }
 
-func indexHandler(writer http.ResponseWriter, request *http.Request) {
+type CaesarText struct {
+	Text string `json:"text"`
+}
+
+type Caesar struct {
+	key int
+}
+
+func NewCaesar(key int) *Caesar {
+	return &Caesar{key}
+}
+
+// Encipher enciphers string using Caesar cipher according to key.
+func (c *Caesar) Encipher(text string) string {
+	return caesarEncipher(text, c.key)
+}
+
+// Decipher deciphers string using Caesar cipher according to key.
+func (c *Caesar) Decipher(text string) string {
+	return caesarEncipher(text, -c.key)
+}
+
+func caesarEncipher(text string, key int) string {
+	if key == 0 {
+		return text
+	}
+	return mapAlpha(text, func(i, char int) int {
+		return char + key
+	})
+}
+
+func mod(a int, b int) int {
+	return (a%b + b) % b
+}
+
+func mapAlpha(text string, f func(i, char int) int) string {
+	runes := []rune(text)
+	for i, char := range runes {
+		if char >= 'A' && char <= 'Z' {
+			runes[i] = rune(mod(f(i, int(char-'A')), 26)) + 'A'
+		} else if char >= 'a' && char <= 'z' {
+			runes[i] = rune(mod(f(i, int(char-'a')), 26)) + 'a'
+		}
+	}
+	return string(runes)
+}
+
+func main() {
+	http.HandleFunc("/atbash", atbashHandler)
+	http.HandleFunc("/caesar", caesarHandler)
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe", err)
+	}
+}
+
+func atbashHandler(writer http.ResponseWriter, request *http.Request) {
 	enableCors(&writer)
-	fmt.Println("GET params were:", request.URL.Query())
 	text := request.URL.Query().Get("text")
 	var cipherText bytes.Buffer
 	for _, char := range text {
@@ -30,18 +86,20 @@ func indexHandler(writer http.ResponseWriter, request *http.Request) {
 			cipherText.WriteRune(char)
 		}
 	}
-	fmt.Println(cipherText.String())
 	atbash := Atbash{cipherText.String()}
 	resp, _ := json.Marshal(atbash)
 	writer.Write(resp)
 }
 
-func main() {
-	http.HandleFunc("/atbash", indexHandler)
-	err := http.ListenAndServe(":8000", nil)
-	if err != nil {
-		log.Fatal("ListenAndServe", err)
-	}
+func caesarHandler(writer http.ResponseWriter, request *http.Request) {
+	enableCors(&writer)
+	fmt.Println("GET params were:", request.URL.Query())
+	text := request.URL.Query().Get("text")
+	key := request.URL.Query().Get("key")
+	ikey, _ := strconv.Atoi(key)
+	caesarText := CaesarText{NewCaesar(ikey).Encipher(text)}
+	resp, _ := json.Marshal(caesarText)
+	writer.Write(resp)
 }
 
 func enableCors(w *http.ResponseWriter) {
